1935b82d99ffe391fde187cc4709fa82cf68b1cf
Italo Ramillys <italoramillys@gmail.com>
2023-07-09 18:10:33 -0300

    Start deletion

--- Trabalho 4/AVL.cpp
+++ Trabalho 4/AVL.cpp
@@ -127,6 +127,110 @@ Noh* inserir(DicAVL& D, TC c, TV v) {
 		return inserir(D.raiz, c, v);
 }
 
+Noh* deleteNode(Node* root, int chave)
+{
+     
+    // STEP 1: PERFORM STANDARD BST DELETE
+    if (root == NULL)
+        return root;
+ 
+    // If the chave to be deleted is smaller
+    // than the root's chave, then it lies
+    // in esq subtree
+    if ( chave < root->chave )
+        root->esq = deleteNode(root->esq, chave);
+ 
+    // If the chave to be deleted is greater
+    // than the root's chave, then it lies
+    // in dir subtree
+    else if( chave > root->chave )
+        root->dir = deleteNode(root->dir, chave);
+ 
+    // if chave is same as root's chave, then
+    // This is the node to be deleted
+    else
+    {
+        // node with only one child or no child
+        if( (root->esq == NULL) ||
+            (root->dir == NULL) )
+        {
+            Node *temp = root->esq ?
+                         root->esq :
+                         root->dir;
+ 
+            // No child case
+            if (temp == NULL)
+            {
+                temp = root;
+                root = NULL;
+            }
+            else // One child case
+            *root = *temp; // Copy the contents of
+                           // the non-empty child
+            free(temp);
+        }
+        else
+        {
+            // node with two children: Get the inorder
+            // successor (smallest in the dir subtree)
+            Node* temp = minValueNode(root->dir);
+ 
+            // Copy the inorder successor's
+            // data to this node
+            root->chave = temp->chave;
+ 
+            // Delete the inorder successor
+            root->dir = deleteNode(root->dir,
+                                     temp->chave);
+        }
+    }
+ 
+    // If the tree had only one node
+    // then return
+    if (root == NULL)
+    return root;
+ 
+    // STEP 2: UPDATE h OF THE CURRENT NODE
+    root->h = 1 + max(h(root->esq),
+                           h(root->dir));
+ 
+    // STEP 3: GET THE BALANCE FACTOR OF
+    // THIS NODE (to check whether this
+    // node became unbalanced)
+    int balance = getBalance(root);
+ 
+    // If this node becomes unbalanced,
+    // then there are 4 cases
+ 
+    // esq esq Case
+    if (balance > 1 &&
+        getBalance(root->esq) >= 0)
+        return dirRotate(root);
+ 
+    // esq dir Case
+    if (balance > 1 &&
+        getBalance(root->esq) < 0)
+    {
+        root->esq = esqRotate(root->esq);
+        return dirRotate(root);
+    }
+ 
+    // dir dir Case
+    if (balance < -1 &&
+        getBalance(root->dir) <= 0)
+        return esqRotate(root);
+ 
+    // dir esq Case
+    if (balance < -1 &&
+        getBalance(root->dir) > 0)
+    {
+        root->dir = dirRotate(root->dir);
+        return esqRotate(root);
+    }
+ 
+    return root;
+}
+
 
 Noh* procurar(Noh* n, TC c) {
 	if (n == nullptr || c == n->chave)
